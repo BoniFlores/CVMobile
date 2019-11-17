d34ffb12b9a624e8739de0c6790b9c48a45aa67f
BONI FLORES <kbeboOficial@gmail.com>
2019-11-16 22:12:37 -0600

    comiint 3

--- app/src/main/java/com/sintaxiserror/cvmobile/MainActivity.java
+++ app/src/main/java/com/sintaxiserror/cvmobile/MainActivity.java
@@ -2,13 +2,26 @@ package com.sintaxiserror.cvmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView inicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicio= findViewById(R.id.tvCV);
+
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
